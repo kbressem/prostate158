4d8afa3e9d40ff464616f3f4de9bbcf104c6a79b
majoreks <s.majorek.konta@gmail.com>
2023-11-12 10:11:29 +0100

    hardcode max workers to 10

--- prostate158/utils.py
+++ prostate158/utils.py
@@ -28,29 +28,31 @@ def load_config(fn: str='config.yaml'):
                              ('nearest', ) * len(config.data.label_cols)
     return config
 
-    
 def num_workers():
-    "Get max supported workers -2 for multiprocessing"
-    import resource
-    import multiprocessing
+    return 10 # depending on the machine, check with lscpu
     
-    # first check for max number of open files allowed on system
-    soft_limit, hard_limit = resource.getrlimit(resource.RLIMIT_NOFILE)
-    n_workers=multiprocessing.cpu_count() - 2
-    # giving each worker at least 256 open processes should allow them to run smoothly
-    max_workers = soft_limit // 256
-    if max_workers < n_workers: 
-        print(
-            "Will not use all available workers as number of allowed open files is to small"
-            "to ensure smooth multiprocessing. Current limits are:\n" 
-            f"\t soft_limit: {soft_limit}\n"
-            f"\t hard_limit: {hard_limit}\n" 
-            "try increasing the limits to at least {256*n_workers}." 
-            "See https://superuser.com/questions/1200539/cannot-increase-open-file-limit-past-4096-ubuntu"
-            "for more details"
-        )
-        return max_workers
+# def num_workers():
+#     "Get max supported workers -2 for multiprocessing"
+#     import resource
+#     import multiprocessing
+    
+#     # first check for max number of open files allowed on system
+#     soft_limit, hard_limit = resource.getrlimit(resource.RLIMIT_NOFILE)
+#     n_workers=multiprocessing.cpu_count() - 2
+#     # giving each worker at least 256 open processes should allow them to run smoothly
+#     max_workers = soft_limit // 256
+#     if max_workers < n_workers: 
+#         print(
+#             "Will not use all available workers as number of allowed open files is to small"
+#             "to ensure smooth multiprocessing. Current limits are:\n" 
+#             f"\t soft_limit: {soft_limit}\n"
+#             f"\t hard_limit: {hard_limit}\n" 
+#             "try increasing the limits to at least {256*n_workers}." 
+#             "See https://superuser.com/questions/1200539/cannot-increase-open-file-limit-past-4096-ubuntu"
+#             "for more details"
+#         )
+#         return max_workers
               
-    return n_workers
+#     return n_workers
 
 USE_AMP=True if monai.utils.get_torch_version_tuple() >= (1, 6) else False
